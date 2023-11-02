47f3a30f43ccc12f74ff6329e6682f759aa7e8eb
MBurton-Johnson <matt.burton-johnson@pwc.com>
2023-11-02 19:33:41 +0000

    adding routes and controllers for update and delete calendar function

--- routes/api/calendar.js
+++ routes/api/calendar.js
@@ -1,4 +1,4 @@
-import { displayCalendar, displayMostRecentWeight, saveCalendar } from "../../controllers/api/calendar.js";
+import { displayCalendar, displayMostRecentWeight, saveCalendar, deleteCalendar, updateCalendar } from "../../controllers/api/calendar.js";
 import express from 'express'
 
 const calendarRouter = express.Router()
@@ -8,5 +8,9 @@ calendarRouter.get('/:id', async (req, res) => displayCalendar(req,res))
 calendarRouter.get('/recent/:id', async (req,res) => displayMostRecentWeight(req, res))
 
 calendarRouter.post('/new', async (req, res) => saveCalendar(req,res))
+
+calendarRouter.put('/update/:id', async (req, res) => updateCalendar(req, res))
+
+calendarRouter.delete('/delete/:id', async (req, res) => deleteCalendar(req, res))
 
 export default calendarRouter
