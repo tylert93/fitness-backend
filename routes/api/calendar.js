dad152046e2f27b26c25db0526942e61f21657f7
jeeivan <jeeivansri@gmail.com>
2023-11-02 09:31:07 +0000

    Updated userSchema

--- routes/api/calendar.js
+++ routes/api/calendar.js
@@ -1,9 +1,11 @@
-import { displayCalendar, saveCalendar } from "../../controllers/api/calendar.js";
+import { displayCalendar, displayMostRecentWeight, saveCalendar } from "../../controllers/api/calendar.js";
 import express from 'express'
 
 const calendarRouter = express.Router()
 
 calendarRouter.get('/:id', async (req, res) => displayCalendar(req,res))
+
+calendarRouter.get('/recent/:id', async (req,res) => displayMostRecentWeight(req, res))
 
 calendarRouter.post('/new', async (req, res) => saveCalendar(req,res))
 
